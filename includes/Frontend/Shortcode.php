5071e31a8f3b251a24f2986fdb9af06d1c2645a0
ratul <ratuljh@gmail.com>
2021-04-13 20:13:56 +0600

    My GitHub.

--- includes/Frontend/Shortcode.php
+++ includes/Frontend/Shortcode.php
@@ -33,30 +33,22 @@ class Shortcode {
     public function __construct() {
         $this->admin_transient = new Transient();
         add_shortcode( 'my_github', array( $this, 'cb_my_github_shortcode' ) );
+        add_action( 'wp_enqueue_scripts', array( $this, 'load_dashicons_front_end' ) );
     }
 
     /**
-     * Callback for My GitHub shortcode
+     * Load dashicons
      *
      * @return void
      */
-    public function cb_my_github_shortcode() {
-        $username = Transient::get_github_username();
-        $response = wp_remote_get( "https://api.github.com/users/{$username}" );
-        $body     = wp_remote_retrieve_body( $response );
-        $body     = json_decode( $body );
-
-        if ( !  $body->message  ) {
-            // echo '<pre>';
-            // print_r( $body );
-            include_once MY_GITHUB_INCLUDE_PATH . '/templates/my_github.php';
-        }
+    public function load_dashicons_front_end() {
+        wp_enqueue_style( 'dashicons' );
     }
 
     /**
      * To get github followers
      *
-     * @param  string $followers github followers.
+     * @param  string $followers  github followers.
      *
      * @return mixed
      */
@@ -66,13 +58,14 @@ class Shortcode {
         }
         $response = wp_remote_get( "$followers" );
         $body     = wp_remote_retrieve_body( $response );
+
         return json_decode( $body );
     }
 
     /**
      * To get github username
      *
-     * @param  string $username github username.
+     * @param  string $username  github username.
      *
      * @return mixed
      */
@@ -82,7 +75,64 @@ class Shortcode {
         }
         $response = wp_remote_get( "$username" );
         $body     = wp_remote_retrieve_body( $response );
+
         return json_decode( $body );
+    }
+
+    /**
+     * To get github following
+     *
+     * @param  string $following_url  github following.
+     *
+     * @return mixed
+     */
+    public static function get_following( string $following_url ) {
+        if ( ! $following_url ) {
+            return false;
+        }
+        $response = wp_remote_get( "$following_url" );
+        $body     = wp_remote_retrieve_body( $response );
+
+        return json_decode( $body );
+    }
+
+    /**
+     * To get github repos_url
+     *
+     * @param  string $repos_url  github repos_url.
+     *
+     * @return mixed
+     */
+    public static function get_repositories( string $repos_url ) {
+        if ( ! $repos_url ) {
+            return false;
+        }
+        $response = wp_remote_get( "$repos_url" );
+        $body     = wp_remote_retrieve_body( $response );
+
+        return json_decode( $body );
+    }
+
+    /**
+     * Callback for My GitHub shortcode
+     *
+     * @return void
+     */
+    public function cb_my_github_shortcode() {
+        // GitHub Token
+        // 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
+        // https://api.github.com/gists/starred?access_token=OAUTH-TOKEN
+        // $response = wp_remote_get( "https://api.github.com/users/{$username['my_github_username']}?access_token=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" );
+
+        $username      = Transient::get_github_username();
+        $response      = wp_remote_get( "https://api.github.com/users/{$username['my_github_username']}" );
+        $body          = wp_remote_retrieve_body( $response );
+        $body          = json_decode( $body );
+        $response_code = wp_remote_retrieve_response_code( $response );
+
+        if ( 200 === $response_code ) {
+            include_once MY_GITHUB_INCLUDE_PATH . '/templates/my_github.php';
+        }
     }
 
 }
--- includes/templates/my_github.php
+++ includes/templates/my_github.php
@@ -11,46 +11,100 @@
 ?>
 <div class="wrap">
     <div class="card">
-        <img class="avatar-image" src="<?php echo esc_url( $body->avatar_url ); ?>" alt="<?php echo esc_html( $body->name ); ?>">
+        <img class="avatar-image" src="<?php echo esc_url( $body->avatar_url ); ?>" alt="<?php echo esc_attr( $body->name ); ?>">
         <div class="container">
-            <h4><b><a href="<?php echo esc_url( $body->url ); ?>" target="_blank"><?php echo esc_html( $body->name ); ?></a></b></h4>
-            <p><?php echo esc_html( $body->bio ); ?></p>
-            <p>
+                <h4>
+                    <b>
+                        <a href="<?php echo esc_url( $body->url ); ?>" target="_blank"><?php echo esc_html( $body->name ); ?></a>
+                    </b>
+                    <small><?php echo esc_html( $body->company ); ?></small>
+                </h4>
+                <small>Bio: <?php echo esc_html( $body->bio ); ?></small>
                 <div class="row">
-                    <b>Followers</b>
-                </div>
+                    <?php
+                        $followers = \My\GitHub\Frontend\Shortcode::get_followers( $body->followers_url );
+                    ?>
+                    <b class="dashicons-groups">Followers: <?php echo esc_html( count( $followers ) ); ?></b>
+                    <hr/>
                 <?php
-                $followers = \My\GitHub\Frontend\Shortcode::get_followers( $body->followers_url );
                 if ( ! empty( $followers ) ) {
 					foreach ( $followers as $follower ) {
-//                        $username = \My\GitHub\Frontend\Shortcode::get_username( $follower->url );
 						?>
                         <a href="<?php echo esc_url( $follower->url ); ?>" target="_blank">
-                            <img title="<?php echo esc_html( $follower->login ); ?>" class="follower_img" src="<?php echo esc_url( $follower->avatar_url ); ?>" alt="<?php echo esc_html( $follower->login ); ?>">
-<!--                            <img title="--><?php //echo esc_html( $username->name ); ?><!--" class="follower_img" src="--><?php //echo esc_url( $username->avatar_url ); ?><!--" alt="--><?php //echo esc_html( $username->name ); ?><!--">-->
+                            <img title="<?php echo esc_attr( $follower->login ); ?>" class="follower_img" src="<?php echo esc_url( $follower->avatar_url ); ?>" alt="<?php echo esc_attr( $follower->login ); ?>">
                         </a>
 						<?php
 					}
                 }
                 ?>
-            </p>
+                </div>
+            <div class="row">
+                <?php
+                    $following_url = str_replace( '{/other_user}', '', $body->following_url );
+                    $following_url = \My\GitHub\Frontend\Shortcode::get_following( $following_url );
+                ?>
+                <b>Following: <?php echo esc_html( count( $following_url ) ); ?></b>
+                <hr/>
+            <?php
+			if ( ! empty( $following_url ) ) {
+				foreach ( $following_url as $following ) {
+					?>
+                        <a href="<?php echo esc_url( $following->url ); ?>" target="_blank">
+                            <img title="<?php echo esc_attr( $following->login ); ?>" class="follower_img" src="<?php echo esc_url( $following->avatar_url ); ?>" alt="<?php echo esc_attr( $following->login ); ?>">
+                        </a>
+						<?php
+				}
+			}
+			?>
+            </div>
+            <div class="row">
+                <b>Repositories</b>
+                <hr/>
+            <?php
+                $repos_url = \My\GitHub\Frontend\Shortcode::get_repositories( $body->subscriptions_url );
+			if ( ! empty( $repos_url ) ) {
+			    echo '<ol>';
+				foreach ( $repos_url as $repos ) {
+					?>
+                        <li>
+                            <a href="<?php echo esc_url( $repos->html_url ); ?>" target="_blank">
+                                <?php echo esc_attr( $repos->name ); ?>
+                            </a>
+                            <?php
+							if ( $repos->fork ) {
+								echo ' (Forked)';
+							}
+                            ?>
+                        </li>
+						<?php
+				}
+                echo '</ol>';
+			}
+			?>
+            </div>
         </div>
     </div>
 </div>
 
 <style>
     .card .avatar-image {
-        width: 100%;
+        width: 50%;
     }
 
     .follower_img {
-        width: 15%;
+        width: 10%;
         border-radius: 50%;
         padding: 5px 10px;
     }
-
+    .row{
+        margin: 20px;
+        padding-bottom: 10px;
+    }
     .card:hover {
         box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+    }
+    small{
+        font-size: 14px;
     }
     .container {
         padding: 5px 20px;
