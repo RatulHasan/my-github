3169a92ea8e3b54b2abae20791e181d054299347
ratul <ratuljh@gmail.com>
2021-04-21 22:58:36 +0600

    Add Github access token

--- includes/Transient.php
+++ includes/Transient.php
@@ -33,6 +33,7 @@ class Transient {
     public function cb_update_option() {
         delete_transient( 'admin_my_github_details' );
         delete_transient( 'my_github_root' );
+        delete_transient( 'my_github_details_cd73a2ae7d0fce9f22d1be0662f2ed40' );
     }
 
     /**
@@ -58,17 +59,40 @@ class Transient {
         $body = get_transient( 'my_github_root' );
         if ( ! $body ) {
             $username = Transient::admin_my_github_details();
-            $response = wp_remote_get( esc_url_raw( "https://api.github.com/users/{$username['my_github_username']}" ) );
-            $body     = wp_remote_retrieve_body( $response );
-            $body     = json_decode( $body );
+            if ( isset( $username['my_github_access_token'] ) && ! empty( $username['my_github_access_token'] ) ) {
+                $url      = 'https://api.github.com/user';
+                $args     = array(
+                    'timeout' => 20,
+                    'headers' => array(
+                        'Authorization' => 'token ' . esc_attr( $username['my_github_access_token'] ),
+                    ),
+                );
+                $response = wp_remote_get( $url, $args );
+            } else {
+                $args     = array(
+                    'timeout' => 20,
+                );
+                $response = wp_remote_get( esc_url_raw( "https://api.github.com/users/{$username['my_github_username']}" ), $args );
+            }
+            $response_code = wp_remote_retrieve_response_code( $response );
 
             if ( is_wp_error( $response ) ) {
                 $error_message = $response->get_error_message();
                 $body          = "Something went wrong: $error_message";
-            } else {
-                set_transient( 'my_github_root', $body, HOUR_IN_SECONDS );
+            }
+
+            if ( 200 === $response_code ) {
+                $body = wp_remote_retrieve_body( $response );
+                $body = json_decode( $body );
+
+                if ( isset( $username['my_github_access_token'] ) && ! empty( $username['my_github_access_token'] ) ) {
+                    set_transient( 'my_github_root', $body, MINUTE_IN_SECONDS );
+                } else {
+                    set_transient( 'my_github_root', $body, HOUR_IN_SECONDS );
+                }
             }
         }
+
         return $body;
     }
 
@@ -86,16 +110,35 @@ class Transient {
             if ( ! $url ) {
                 return false;
             }
-            $response      = wp_remote_get( esc_url_raw( "$url" ) );
+
+            $username = Transient::admin_my_github_details();
+            $args     = array(
+                'timeout' => 20,
+            );
+            if ( isset( $username['my_github_access_token'] ) && ! empty( $username['my_github_access_token'] ) ) {
+                $args = array(
+                    'headers' => array(
+                        'Authorization' => 'token ' . esc_attr( $username['my_github_access_token'] ),
+                    ),
+                );
+            }
+            $response      = wp_remote_get( $url, $args );
             $response_code = wp_remote_retrieve_response_code( $response );
 
-            if ( 200 === $response_code ) {
-                $body = wp_remote_retrieve_body( $response );
-                $body = json_decode( $body );
-            } else {
-                $body = '';
+            if ( is_wp_error( $response ) ) {
+                $error_message = $response->get_error_message();
+                $body          = "Something went wrong: $error_message";
             }
-            set_transient( "my_github_details_{$cash_key}", $body, HOUR_IN_SECONDS );
+
+			if ( 200 === $response_code ) {
+				$body = wp_remote_retrieve_body( $response );
+				$body = json_decode( $body );
+
+                if ( isset( $username['my_github_access_token'] ) && ! empty( $username['my_github_access_token'] ) ) {
+					set_transient( "my_github_details_{$cash_key}", $body, MINUTE_IN_SECONDS );
+				}
+				set_transient( "my_github_details_{$cash_key}", $body, HOUR_IN_SECONDS );
+			}
         }
         return $body;
     }
