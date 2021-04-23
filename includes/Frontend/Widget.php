bf22bb108d8e06280d14e8922755b9ce1597d077
rajankarmakar <rajankarmakar@hotmail.com>
2021-04-23 20:08:53 +0600

    Included wiget form template

--- includes/Frontend/Widget.php
+++ includes/Frontend/Widget.php
@@ -89,17 +89,7 @@ class Widget extends \WP_Widget {
         $title = ( ! empty( $instance['title'] ) ) ? esc_attr( $instance['title'] ) : '';
         $show_avatar = ( ! empty( $instance['show_avatar'] ) ) ? esc_attr( $instance['show_avatar'] ) : '';
 
-        ?>
-        <p>
-			<label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php echo esc_html__( 'Title:', 'my-github' ); ?></label>
-			<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo $title; ?>">
-		</p>
-        <p>
-            <label for="<?php echo esc_attr( $this->get_field_id( 'show_avatar' ) ); ?>">
-            <input name="<?php echo esc_attr( $this->get_field_name( 'show_avatar' ) ); ?>" type="checkbox" id="<?php echo esc_attr( $this->get_field_id( 'show_avatar' ) ); ?>" value="1" <?php checked( "1" === $show_avatar, 1 ); ?>><?php echo esc_html__( 'Show/Hide profile avatar' ); ?></label>
-        </p>
-        <?php
-
+        include_once MY_GITHUB_INCLUDE_PATH . '/templates/my_github_widget_form.php';
     }
 
     /**
