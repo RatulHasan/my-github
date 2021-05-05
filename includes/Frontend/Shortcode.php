ac077ffd0ab50234910eb815ffcf30bdcf11c803
ratul <ratuljh@gmail.com>
2021-05-06 03:02:14 +0600

    Template include added.

--- includes/Frontend/Shortcode.php
+++ includes/Frontend/Shortcode.php
@@ -23,16 +23,41 @@ class Shortcode {
     /**
      * For storing AdminTransient instance
      *
-     * @var Transient $admin_transient
+     * @since 1.2.2
+     *
+     * @var array $admin_details
      */
-    public $admin_transient;
+    public $admin_details;
 
     /**
-     * Frontend constructor.
+     * Shortcode constructor.
      */
     public function __construct() {
-        $this->admin_transient = new Transient();
+        $this->admin_details = Transient::admin_my_github_details();
+        if ( empty( $this->admin_details['my_github_username'] ) ) {
+            return false;
+        }
+        if ( isset( $this->admin_details['is_show_in_custom_template'] ) && $this->admin_details['is_show_in_custom_template'] ) {
+            add_filter( 'template_include', array( $this, 'cb_template_include' ) );
+        }
         add_shortcode( 'my_github', array( $this, 'cb_my_github_shortcode' ) );
+    }
+
+    /**
+     * Callback for template include.
+     *
+     * @param string $template current template.
+     *
+     * @since 1.2.2
+     *
+     * @return string
+     */
+    public function cb_template_include( $template ) {
+        global $post;
+        if ( has_shortcode( $post->post_content, 'my_github' ) ) {
+            $template = apply_filters( 'my_github_custom_template', MY_GITHUB_INCLUDE_PATH . '/templates/my_github_main.php' );
+        }
+        return $template;
     }
 
     /**
@@ -41,8 +66,7 @@ class Shortcode {
      * @return false|void
      */
     public function cb_my_github_shortcode() {
-        $my_github_details = Transient::admin_my_github_details();
-        if ( empty( $my_github_details['my_github_username'] ) ) {
+        if ( empty( $this->admin_details['my_github_username'] ) ) {
             return false;
         }
 
